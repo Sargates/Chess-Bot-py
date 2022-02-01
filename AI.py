923187ce14b5d7fbd065be712186242b8790eae4
Sargates <nicholas.glenn@nicholas-glenn.local>
2022-02-01 15:55:30 -0600

    changed AI.py

--- AI.py
+++ AI.py
@@ -1,5 +1,27 @@
 from board import Board
+from pieces.move import Move
+import random
 
 class AI:
-	def __init__(self) -> None:
-		pass
+	def __init__(self, board :Board) -> None:
+		self.board = Board()
+
+		self.board.__dict__ == board.__dict__
+
+	def print(self):
+		string = ""
+		for i in range(8):
+			for j in range(8):
+				string += str(self.board.board[i * 8 + j]) + " "
+			string += "\n"
+		
+		print(string)
+	
+	def search(self, depth):
+		allMoves = self.board.getAllMoves("b")
+
+		index = random.randint(0, len(allMoves)-1)
+		selectedMove = allMoves[index]
+
+		selectedMove.makeMove()
+		
